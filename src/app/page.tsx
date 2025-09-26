c360b0fca4ae940250aef5d1b95444e0b362dfa4
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-09-27 00:49:20 +0530

    first commit, file structure half done

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Home() {
+  return (
+    <div className="mx-auto px-4 sm:px-6 lg:px-32 max-w-5xl mt-8">
+      <div className="text-justify">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur provident ad eaque laborum odit dicta quia accusamus odio, deserunt soluta veniam ipsum necessitatibus placeat quis perspiciatis enim praesentium aliquam fugit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+      </div>
+    </div>
+  );
+}
