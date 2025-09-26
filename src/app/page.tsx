7ae98001171794c30725da794f7d5521725738b0
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-09-27 01:01:24 +0530

    added the shadcn/aceternity ui in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
+import { BackgroundLines } from "../components/ui/background-lines";
 
 
 export default function Home() {
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-32 max-w-5xl mt-8">
       <div className="text-justify">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur provident ad eaque laborum odit dicta quia accusamus odio, deserunt soluta veniam ipsum necessitatibus placeat quis perspiciatis enim praesentium aliquam fugit?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quasi omnis veritatis, neque temporibus corrupti enim ut. In aliquid itaque dicta voluptates voluptas! Temporibus vitae porro ab velit, nam voluptate!
+        <BackgroundLines>
+          <div className="relative z-10 text-center">
+            <h1 className="text-4xl font-bold">Hello World</h1>
+            <p className="text-lg">This is inside BackgroundLines</p>
+          </div>
+        </BackgroundLines>
       </div>
     </div>
   );
