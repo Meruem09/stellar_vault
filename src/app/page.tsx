1ed3f08ebc5d5de6c2717ae297627c63cb9f54a3
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-10-17 20:51:14 +0530

    made minor design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import { BackgroundLines } from "../components/ui/background-lines";
 
-
 export default function Home() {
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-32 max-w-5xl mt-8">
-      <div className="text-justify">
-        <BackgroundLines>
-          <div className="relative z-10 text-center justify-center items-center mt-[45%]">
-            <h1 className="text-4xl font-bold">Hello World</h1>
-            <p className="text-lg">This is inside BackgroundLines</p>
+      <div className=" text-justify">
+          <div className="relative z-10 flex text-black-900 text-center bg-[#e0f2fe] justify-between h-screen p-3 ">
+            <h1 className="text-4xl font-bold ">Rahul Varma</h1>
+            <p className="text-lg">Builder who knows how to build Products</p>
           </div>
-        </BackgroundLines>
       </div>
     </div>
   );
