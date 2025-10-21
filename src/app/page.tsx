ce4b9296e671faf6cb2e28c6c2086a4122ed6b39
Rahul Varma <167012900+Meruem09@users.noreply.github.com.>
2025-10-21 19:20:47 +0530

    added root page n structure, features n data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,98 @@
-import { BackgroundLines } from "../components/ui/background-lines";
+import dayjs from "dayjs";
+import type { ProfilePage as PageSchema, WithContext } from "schema-dts";
 
-export default function Home() {
+import { About } from "@/features/profile/components/about";
+import { Awards } from "@/features/profile/components/awards";
+import { Blog } from "@/features/profile/components/blog";
+import { Brand } from "@/features/profile/components/brand";
+import { Certifications } from "@/features/profile/components/certifications";
+import { Experiences } from "@/features/profile/components/experiences";
+import { GitHubContributions } from "@/features/profile/components/github-contributions";
+import { Overview } from "@/features/profile/components/overview";
+import { ProfileCover } from "@/features/profile/components/profile-cover";
+import { ProfileHeader } from "@/features/profile/components/profile-header";
+import { Projects } from "@/features/profile/components/projects";
+import { SocialLinks } from "@/features/profile/components/social-links";
+import { TeckStack } from "@/features/profile/components/teck-stack";
+import { USER } from "@/features/profile/data/user";
+import { cn } from "@/lib/utils";
+
+export default function Page() {
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-32 max-w-5xl mt-8">
-      <div className=" text-justify">
-          <div className="relative z-10 flex text-black-900 text-center bg-[#e0f2fe] justify-between h-screen p-3 ">
-            <h1 className="text-4xl font-bold ">Rahul Varma</h1>
-            <p className="text-lg">Builder who knows how to build Products</p>
-          </div>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(getPageJsonLd()).replace(/</g, "\\u003c"),
+        }}
+      />
+
+      <div className="mx-auto md:max-w-3xl">
+        <ProfileCover />
+        <ProfileHeader />
+        <Separator />
+
+        <Overview />
+        <Separator />
+
+        <SocialLinks />
+        <Separator />
+
+        <About />
+        <Separator />
+
+        <GitHubContributions />
+        <Separator />
+
+        <TeckStack />
+        <Separator />
+
+        <Blog />
+        <Separator />
+
+        <Experiences />
+        <Separator />
+
+        <Projects />
+        <Separator />
+
+        <Awards />
+        <Separator />
+
+        <Certifications />
+        <Separator />
+
+        <Brand />
+        <Separator />
       </div>
-    </div>
+    </>
+  );
+}
+
+function getPageJsonLd(): WithContext<PageSchema> {
+  return {
+    "@context": "https://schema.org",
+    "@type": "ProfilePage",
+    dateCreated: dayjs(USER.dateCreated).toISOString(),
+    dateModified: dayjs().toISOString(),
+    mainEntity: {
+      "@type": "Person",
+      name: USER.displayName,
+      identifier: USER.username,
+      image: USER.avatar,
+    },
+  };
+}
+
+function Separator({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        "relative flex h-8 w-full border-x border-edge",
+        "before:absolute before:-left-[100vw] before:-z-1 before:h-8 before:w-[200vw]",
+        "before:bg-[repeating-linear-gradient(315deg,var(--pattern-foreground)_0,var(--pattern-foreground)_1px,transparent_0,transparent_50%)] before:bg-size-[10px_10px] before:[--pattern-foreground:var(--color-edge)]/56",
+        className
+      )}
+    />
   );
 }
